ca0a3e3f391a77bca6c047bc854adc699bd89067
alv4rol3on <119714425+alv4rol3on@users.noreply.github.com>
2024-01-24 09:53:23 -0500

    Add files via upload

---
+++ proyectoColegio/app.py
@@ -0,0 +1,67 @@
+import mysql.connector
+from flask import *
+from flask_wtf import *
+from forms import agregarAlumno, agregarProfe
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mikeysecreta'
+
+#conexion a base de datos
+def conectar():
+    conexion = mysql.connector.connect(
+        user="root",
+        password="",
+        host="localhost",
+        database="proyectocole",
+        port="3307")
+    cursor = conexion.cursor()
+    return conexion, cursor
+
+
+#paginas
+@app.route("/agregarAlumno", methods=["GET", "POST"])
+def formularioAlumnos():
+    aga = agregarAlumno(request.form)
+    if request.method == "POST" and aga.validate:
+        nombre = aga.nombre.data
+        apellido = aga.apellido.data
+        grado = aga.grado.data
+        seccion = aga.seccion.data
+        
+        # registro en la base de datos
+        try:
+            conexion, cursor = conectar()
+            cursor.execute(
+                f"""
+                INSERT INTO `alumnos`(`nombre`, `apellido`, `grado`, `seccion`) VALUES ('{aga.nombre.data}','{aga.apellido.data}','{aga.grado.data}','{aga.seccion.data}')
+                """)
+            conexion.commit()
+            return redirect(url_for("inicio.html"))
+        except:
+            print("ERROR A: ERROR AL INSERTAR UN NUEVO ALUMNO")
+        finally:
+            conexion.close()
+            return redirect("inicio")
+        
+    return render_template("agregarAlumno.html", aga=aga)
+
+
+@app.route("/agregarProfesor", methods=["GET", "POST"])
+def formularioProfesor():
+    agp = agregarProfe(request.form)
+    if request.form == "POST" and agp.validate:
+        nombre = agp.nombre.data
+        apellido = agp.apellido.data
+        telefono = agp.telefono.data
+        email = agp.email.data
+        salon = agp.salon.data
+ 
+    return render_template("agregarProfesor.html", agp=agp)
+        
+
+@app.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
+    
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ proyectoColegio/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField, EmailField, SelectField
+from wtforms.validators import DataRequired, Email
+
+class agregarProfe(FlaskForm):
+    nombre = StringField("Nombre: ", validators=[DataRequired()], render_kw={"placeholder": "Ingrese el nombre"})
+    apellido = StringField("Apellido: ", validators=[DataRequired()], render_kw={"placeholder": "Ingrese el apellido"})
+    telefono = IntegerField("Telefono: ", validators=[DataRequired()], render_kw={"placeholder": "Ingrese el telefono"})
+    email = EmailField("Correo electronico: ", validators=[DataRequired(), Email()], render_kw={"placeholder": "Ingrese el correo electronico"})
+    salon = SelectField("Salón de tutoria: ", choices=[("Libre", "Libre"), ("3 años", "3 años"), ("4 años", "4 años"), ("5 años", "5 años"),
+                                                       ("1ro prim", "1ro prim"), ("2do prim", "2do prim"), 
+                                                       ("3ro prim", "3ro prim"), ("4to prim", "4to prim"),
+                                                       ("5to prim", "5to prim"), ("6to prim", "6to prim"),
+                                                       ("1ro sec", "1ro sec"), ("2do sec", "2do sec"),
+                                                       ("3ro sec", "3ro sec"), ("4to sec", "4to sec"),
+                                                       ("5to sec", "5to sec")])   
+    boton = SubmitField("Registrar nuevo docente")
+    
+class agregarAlumno(FlaskForm):
+    nombre = StringField("Nombre: ", validators=[DataRequired()])
+    apellido = StringField("Apellido: ", validators=[DataRequired()])
+    grado = SelectField("Grado del alumno: ", choices=[("3 años", "3 años"), ("4 años", "4 años"), ("5 años", "5 años"),
+                                                       ("1ro prim", "1ro prim"), ("2do prim", "2do prim"), 
+                                                       ("3ro prim", "3ro prim"), ("4to prim", "4to prim"),
+                                                       ("5to prim", "5to prim"), ("6to prim", "6to prim"),
+                                                       ("1ro sec", "1ro sec"), ("2do sec", "2do sec"),
+                                                       ("3ro sec", "3ro sec"), ("4to sec", "4to sec"),
+                                                       ("5to sec", "5to sec")])   
+    seccion = SelectField("Seccion: ", choices=[("A", "A"), ("B", "B"),
+                                               ("C", "C"), ("D", "D"),
+                                               ("E", "E"), ("F", "F")])   
+    boton = SubmitField("Registrar nuevo alumno")
