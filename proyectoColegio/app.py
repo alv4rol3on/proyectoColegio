76e770035a7268cd8f9df719b6a0a447b628feba
alv4rol3on <alv4rol3on@gmail.com>
2024-01-26 20:43:33 -0500

    ACTUALIZACIÓN 26/01 9:00PM
    - 1er formulario de profesores
    - Se retiró código no necesario o redundante
    - Se ordenó mejor el contenido de la carpeta de templates

--- proyectoColegio/app.py
+++ proyectoColegio/app.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from flask import *
 from flask_wtf import *
-from forms import agregarAlumno, agregarProfe, borrar
+from forms import losforms, borrar
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mikeysecreta'
@@ -17,17 +17,16 @@ def conectar():
     cursor = conexion.cursor(dictionary=True)
     return conexion, cursor
 
-
 #PAGINAS CON FORMULARIO ALUMNO
 @app.route("/agregarAlumno", methods=["GET", "POST"])
 def formularioAlumnos():
-    aga = agregarAlumno(request.form)
+    aga = losforms(request.form)
     if request.method == "POST" and aga.validate:
         nombre = aga.nombres.data
         apellidoPa = aga.apellidoPaterno.data
         apellidoMa = aga.apellidoMaterno.data
         dni = aga.dni.data
-        nroApo = aga.nroApoderado.data
+        nroApo = aga.nro.data
         grado = aga.grado.data
         seccion = aga.seccion.data
         
@@ -37,7 +36,7 @@ def formularioAlumnos():
             cursor.execute(
                 f"""
                 INSERT INTO `proyectocole`.`alumno` (`nombreCompleto`, `tipoUsuario`, `dni`, `telf`, `grado`, `seccion`, `estado`)
-                VALUES ('{aga.apellidoPaterno.data + " "+ aga.apellidoMaterno.data+ ", " + aga.nombres.data}', '{3}', '{aga.dni.data}', '{aga.nroApoderado.data}', '{aga.grado.data}', '{aga.seccion.data}', 'alumno');
+                VALUES ('{aga.apellidoPaterno.data + " "+ aga.apellidoMaterno.data+ ", " + aga.nombres.data}', '{3}', '{aga.dni.data}', '{aga.nro.data}', '{aga.grado.data}', '{aga.seccion.data}', 'alumno');
                 """)
             conexion.commit()
         except:
@@ -46,7 +45,7 @@ def formularioAlumnos():
             conexion.close()
             return redirect(url_for("inicio"))
         
-    return render_template("agregarAlumno.html", aga=aga) 
+    return render_template("alumnos/agregarAlumno.html", aga=aga) 
 
 @app.route("/borrarAlumno", methods=["GET", "POST"])
 def borrarAlumnos():
@@ -68,27 +67,74 @@ def borrarAlumnos():
             conexion.close()
             return redirect(url_for("inicio"))
             
-    return render_template("borrarAlumno.html", ba=ba)    
+    return render_template("alumnos/borrarAlumno.html", ba=ba)    
 
 @app.route("/listaDeAlumnos", methods=["GET", "POST"])     
 def listaAlumnos():
     listado = listar()
-    return render_template("listaAlumnos.html", listado = listado)
+    return render_template("alumnos/listaAlumnos.html", listado = listado)
+ 
+@app.route("/actualizarAlumno", methods=["GET", "POST"])
+def actAlumnos():
+    act = losforms(request.form)
+    if request.method == "POST" and act.validate:
+        id = act.id.data
+        dni = act.dni.data  
+        nombre = act.nombres.data
+        apellidoPa = act.apellidoPaterno.data
+        apellidoMa = act.apellidoMaterno.data
+        nroApo = act.nro.data
+        grado = act.grado.data
+        seccion = act.seccion.data
+        estado = act.seccion.data
+        
+        # registro en la base de datos
+        try:
+            conexion, cursor = conectar()
+            cursor.execute(
+                f"""
+               UPDATE `proyectocole`.`alumno`
+                SET `nombreCompleto` = '{act.apellidoPaterno.data + " "+ act.apellidoMaterno.data+ ", " + act.nombres.data}', `dni` = {act.dni.data},
+                `telf` = {act.nro.data}, `grado` = '{act.grado.data}', `seccion` = '{act.seccion.data}', `estado` = '{act.estado.data}'
+                WHERE `id` = {act.id.data};
+                """)
+            conexion.commit()
+        except:
+            print("ERROR D: ERROR AL EDITAR UN ALUMNO")
+        finally:
+            conexion.close()
+            return redirect(url_for("inicio"))
+        
+    return render_template("alumnos/actualizarAlumno.html", act=act)
          
 #PAGINAS CON FORMULARIO PROFESOR
 @app.route("/agregarProfesor", methods=["GET", "POST"])
 def formularioProfesor():
-    agp = agregarProfe(request.form)
+    agp = losforms(request.form)
     if request.form == "POST" and agp.validate:
-        nombre = agp.nombre.data
-        apellido = agp.apellido.data
-        telefono = agp.telefono.data
-        email = agp.email.data
-        salon = agp.salon.data
+        nombres = agp.nombres.data
+        dni = agp.dni.data
+        nro = agp.nro.data
+        grado = agp.grado.data
+        #cursosSeleccionados = request.form.getlist('cursos')
+        #listaCursos = ', '.join(cursosSeleccionados)
+        
+        #conexion a base de datos
+        try:
+            conexion, cursor = conectar()
+            cursor.execute(
+                f"""
+                INSERT INTO `proyectocole`.`profesor` (`nombreCompleto`, `tipoUsuario`, `dni`, `telefono`, `tutoria`, `estado`)
+                VALUES('{agp.nombres.data}', '{2}', '{agp.dni.data}', '{agp.nro.data}', '{agp.grado.data}', 'profesor');
+                """)
+            conexion.commit()  
+        except:
+            print("ERROR A2: ERROR AL INSERTAR UN NUEVO PROFESOR")   
+        finally:
+            conexion.close()
+            return redirect(url_for("inicio"))      
  
     return render_template("agregarProfesor.html", agp=agp)
-        
-
 
 #paginas enlace
 @app.route("/")
@@ -112,6 +158,9 @@ def listar():
     conexion.close()
         
     return lista    
-        
+      
+
+
+            
 if __name__ == '__main__':
     app.run(debug=True)
--- proyectoColegio/forms.py
+++ proyectoColegio/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, EmailField, SelectField
+from wtforms import StringField, IntegerField, SubmitField, EmailField, SelectField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired, Email, Length
 
 salones = [("3 años", "3 años"), ("4 años", "4 años"), ("5 años", "5 años"), ("1ro prim", "1ro prim"), ("2do prim", "2do prim"), 
@@ -10,23 +10,19 @@ tutoria = [("libre", "libre"), ("3 años", "3 años"), ("4 años", "4 años"), (
             ("3ro prim", "3ro prim"), ("4to prim", "4to prim"), ("5to prim", "5to prim"), ("6to prim", "6to prim"),
             ("1ro sec", "1ro sec"), ("2do sec", "2do sec"), ("3ro sec", "3ro sec"), ("4to sec", "4to sec"), ("5to sec", "5to sec")]
 
-class agregarProfe(FlaskForm):
-    nombres = StringField(validators=[DataRequired()], render_kw={"placeholder": "Ingrese el nombre"})
-    apellido = StringField(validators=[DataRequired()], render_kw={"placeholder": "Ingrese los apellidos", "size":30})
-    telefono = IntegerField(validators=[DataRequired()], render_kw={"placeholder": "Ingrese el telefono"})
-    email = EmailField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Ingrese el correo electronico", "size":30})
-    salon = SelectField("Salón de tutoria: ", choices=tutoria)   
-    boton = SubmitField("Registrar nuevo docente")
-    
-class agregarAlumno(FlaskForm):
-    nombres = StringField(validators=[DataRequired()], render_kw={"placeholder": "Ingrese los nombres del estudiante", "size":30})
+materias = [("aritmetica", "aritmetica"), ("algebra", "algebra"), ("geometria", "geometria"), ("trigonometria", "trigonometria")]
+       
+class losforms(FlaskForm):
+    id = IntegerField(validators=[DataRequired()], render_kw={"placeholder": "Ingrese el id"}) 
+    nombres = StringField(validators=[DataRequired()], render_kw={"placeholder": "Ingrese los nombres", "size":30})
     apellidoPaterno = StringField(validators=[DataRequired()], render_kw={"placeholder": "Ingrese el apellido paterno"})
     apellidoMaterno = StringField(validators=[DataRequired()], render_kw={"placeholder": "Ingrese el apellido materno"})
     dni = IntegerField(validators=[DataRequired(), Length(min=9, max=9)], render_kw={"placeholder": "Ingrese el dni"})
     grado = SelectField("Grado del alumno: ", choices=salones)   
     seccion = SelectField("Seccion: ", choices=[("A", "A"), ("B", "B")])
-    nroApoderado = IntegerField(validators=[DataRequired()], render_kw={"placeholder": "Telefono del padre, madre o apoderado"})   
-    boton = SubmitField("Registrar nuevo alumno")
+    nro = IntegerField(validators=[DataRequired()], render_kw={"placeholder": "Telefono"})   
+    estado = SelectField("Estado: ", choices=[("alumno", "alumno"), ("suspendido", "suspendido"), ("retirado", "retirado")])
+    boton = SubmitField("Registrar")
     
 class borrar(FlaskForm):
     id = IntegerField("Id de la persona: ", validators=[DataRequired()])  
@@ -35,3 +31,5 @@ class borrar(FlaskForm):
 class filtro(FlaskForm):
     grado = SelectField("Grado del alumno: ", choices=salones)    
     buscar = SubmitField("Buscar")
+    
+   
